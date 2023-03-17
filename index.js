8ed5ef806aee155d0b0a9e95f8d0e2d3fff595c1
utdsi <utkarshsinha852@gmail.com>
2023-03-17 08:13:27 +0530

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app =express()
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("Welcome")
+})
+
+app.listen(8000,()=>{
+    console.log("connected to server")
+})
